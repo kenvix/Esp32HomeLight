344a698534f39485643f90326e71a32f75acab71
Kenvix Zure <kenvixzure@live.com>
2022-01-25 12:23:26 +0800

    Refactor dir structure

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import os
 import sys 
 import log
 import network
-import config
+from config import netconfig
 import time
 import install
 from network import WLAN
@@ -14,18 +14,18 @@ ap_if: WLAN = None
 
 
 def setupAP():
-    log.info("Setting up AP with SSID %s     Key %s" % (config.DEVICE_NAME, config.AP_WPA_KEY))
+    log.info("Setting up AP with SSID %s     Key %s" % (netconfig.DEVICE_NAME, netconfig.AP_WPA_KEY))
     global ap_if
     ap_if = network.WLAN(network.AP_IF)
     ap_if.active(True)
-    ap_if.config(essid=config.DEVICE_NAME, password=config.AP_WPA_KEY)
-    if len(config.AP_WPA_KEY) > 8:
+    ap_if.config(essid=netconfig.DEVICE_NAME, password=netconfig.AP_WPA_KEY)
+    if len(netconfig.AP_WPA_KEY) > 8:
         ap_if.config(authmode = network.AUTH_WPA_WPA2_PSK)
     else:
         log.warn("No AP password set or password is too short")
 
     # No network, not a gateway
-    ap_if.ifconfig((config.AP_IP, '255.255.255.0', config.AP_GATEWAY, config.AP_DNS))
+    ap_if.ifconfig((netconfig.AP_IP, '255.255.255.0', netconfig.AP_GATEWAY, netconfig.AP_DNS))
     log.info("AP Config: %s" % str(ap_if.ifconfig()))
 
 
@@ -34,7 +34,7 @@ def waitAPUp():
     while ap_if.active() == False:
         time.sleep(0.3)
         pass
-    log.info("AP %s is up" % config.DEVICE_NAME)
+    log.info("AP %s is up" % netconfig.DEVICE_NAME)
 
 
 def setupFTP():
@@ -49,22 +49,23 @@ def setupFTP():
 
 def setupSTA():
     global sta_if 
-    if len(config.STA_SSID) < 1:
+    if len(netconfig.STA_SSID) < 1:
         log.info("STA not configured, skip")
     else:
-        log.info("Setting up STA with SSID %s     Key %s" % (config.STA_SSID, config.STA_WPA_KEY))
+        log.info("Setting up STA with SSID %s     Key %s" % (netconfig.STA_SSID, netconfig.STA_WPA_KEY))
         sta_if = network.WLAN(network.STA_IF)
         sta_if.active()
-        sta_if.connect(config.STA_SSID, config.STA_WPA_KEY)
+        sta_if.connect(netconfig.STA_SSID, netconfig.STA_WPA_KEY)
 
 
 def waitSTAUp():
     global sta_if 
-    while sta_if.isconnected() == False:
-        time.sleep(0.3)
-        pass
-    log.info("STA %s is up" % config.STA_SSID)
-    log.info("STA Connection info: %s" % str(sta_if.ifconfig()))
+    if sta_if is not None:
+        while sta_if.isconnected() == False:
+            time.sleep(0.3)
+            pass
+        log.info("STA %s is up" % netconfig.STA_SSID)
+        log.info("STA Connection info: %s" % str(sta_if.ifconfig()))
 
 
 def main():
@@ -82,6 +83,13 @@ def main():
         setupFTP()
     except Exception as e:
         log.error("Setup FTP server FAILED!")
+        sys.print_exception(e, sys.stderr)
+
+    try:
+        setupSTA()
+        waitSTAUp()
+    except Exception as e:
+        log.error("Setup Wi-FI STA FAILED!")
         sys.print_exception(e, sys.stderr)
 
     pass

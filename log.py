7c90e744e8425e04d9887961f8867e3e2e6b8b8a
Kenvix Zure <kenvixzure@live.com>
2022-01-25 14:04:22 +0800

    Log formatting

--- log.py
+++ log.py
@@ -1,15 +1,29 @@
 import sys
 import time
+from machine import RTC
+
+rtc = RTC()
 
 def eprint(*args, **kwargs):
     print(*args, file=sys.stderr, **kwargs)
 
+
+def now():
+    """
+    ( year,month,day,weekday,hour,minute,second,microsecond )
+    """
+    return rtc.datetime()
+
+def nowInString():
+    n = now()
+    return "%04d-%02d-%02d %02d:%02d:%02d.%06d" % (n[0], n[1], n[2], n[4], n[5], n[6], n[7])
+
 def print_log(level, message):
     t = time.time()
-    # if int(t) < 696432783:
-    #     t = str(t)
-    # else:
-    #     t = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
+    if int(t) < 696432783:
+        t = str(t)
+    else:
+        t = nowInString()
 
     eprint("[%s][%s] %s" % (t, level, str(message)))
 
